d07edd37d4edf35740cc2c56b1d861d32b8972aa
sergioelenes <sergioelenes@gmail.com>
2024-01-24 13:05:18 -0700

    xxxxxx
    xxxxxxxxxxxconext

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ app.config['BASIC_AUTH_USERNAME'] = 'brad'
 app.config['BASIC_AUTH_PASSWORD'] = 'keonda'
 basic_auth = BasicAuth(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:e96206c6e9b8f5acf6f0d4863ce4cf8a@dokku-postgres-brad:5432/brad'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:e96206c6e9b8f5acf6f0d4863ce4cf8a@198.251.66.139:31173/brad'
 db = SQLAlchemy(app)
 
 
